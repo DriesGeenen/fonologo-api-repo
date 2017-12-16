028cfb709fbdcaf107fd73f20afccd65b5f56fc5
Dries Geenen <driesgeenen@gmail.com>
2017-12-16 20:39:47 +0100

    ...

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports={
-    database: 'mongodb://admin:adminwachtwoordisgeheim@172.30.181.199:27017/mongodb',
+    database: 'mongodb://admin:adminwachtwoordisgeheim@172.30.181.199:27017/project-td',
     secret:'notpublic'
 };
